f8ab80d0e3ab2357fd2cd314fcacf2cce07e3c61
samuilz <samuilzahariev96@gmail.com>
2018-12-18 15:03:04 +0200

    Robot simulator solution.

---
+++ src/robotsimulator/RobotSimulator.java
@@ -0,0 +1,146 @@
+package robotsimulator;
+
+import java.util.Scanner;
+
+
+public class RobotSimulator {
+    private static final String NORTH = "NORTH";
+    private static final String EAST = "EAST";
+    private static final String WEST = "WEST";
+    private static final String SOUTH = "SOUTH";
+    private static final String RIGHT = "R";
+    private static final String LEFT = "L";
+    private static final String ADVANCE = "A";
+    private static final String EMPTY_STRING = "";
+    private static int directionX = 0;
+    private static int directionY = 0;
+    private static int x = 0;
+    private static int y = 0;
+    private static String instructions;
+
+    public static void main(String[] args) {
+        readInput();
+
+        processInstructions();
+
+        printResult();
+    }
+
+    private static void readInput() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Please enter starting X coordinate: ");
+        x = Integer.parseInt(in.nextLine());
+        System.out.println("Please enter starting Y coordinate: ");
+        y = Integer.parseInt(in.nextLine());
+
+        System.out.println("Please enter facing direction");
+        System.out.println("It should be EAST, WEST, NORTH or SOUTH: ");
+        String direction = in.nextLine();
+
+        initDirections(direction);
+
+        System.out.println("Please enter your instructions");
+        System.out.println("They should be in format RAL (e.g. RAALAL) where R stands for right, " +
+                "L stands for Left and A stands for Advance: ");
+        instructions = in.nextLine();
+    }
+
+    private static void initDirections(String direction) {
+        switch (direction) {
+            case NORTH:
+                directionX = 0;
+                directionY = 1;
+                break;
+            case EAST:
+                directionX = 1;
+                directionY = 0;
+                break;
+            case SOUTH:
+                directionX = 0;
+                directionY = -1;
+                break;
+            case WEST:
+                directionX = -1;
+                directionY = 0;
+                break;
+        }
+    }
+
+    private static void processInstructions() {
+        for (int index = 0; index < instructions.length(); index++) {
+            String currentCommand = instructions.substring(index, index + 1);
+
+            switch (currentCommand) {
+                case RIGHT:
+                    turnRight();
+                    break;
+                case LEFT:
+                    turnLeft();
+                    break;
+                case ADVANCE:
+                    changeCoordinates();
+                    break;
+            }
+        }
+    }
+
+    private static void turnRight() {
+        if (directionX == 1) {
+            directionX = 0;
+            directionY = -1;
+        } else if (directionX == -1) {
+            directionX = 0;
+            directionY = 1;
+        } else if (directionY == 1) {
+            directionY = 0;
+            directionX = 1;
+        } else if (directionY == -1) {
+            directionY = 0;
+            directionX = -1;
+        }
+    }
+
+    private static void turnLeft() {
+        if (directionX == 1) {
+            directionX = 0;
+            directionY = 1;
+        } else if (directionX == -1) {
+            directionX = 0;
+            directionY = -1;
+        } else if (directionY == 1) {
+            directionY = 0;
+            directionX = -1;
+        } else if (directionY == -1) {
+            directionY = 0;
+            directionX = 1;
+        }
+    }
+
+    private static void changeCoordinates() {
+        x = x + directionX;
+        y = y + directionY;
+    }
+
+    private static void printResult() {
+        System.out.printf("Final coordinates are: (%d, %d)\n", x, y);
+        String finalDirection = calculateFinalDirection();
+        System.out.printf("The robot now facing %s", finalDirection);
+    }
+
+    private static String calculateFinalDirection() {
+        String finalDirection = EMPTY_STRING;
+
+        if (directionX == 1) {
+            finalDirection = EAST;
+        } else if (directionX == -1) {
+            finalDirection = WEST;
+        } else if (directionY == 1) {
+            finalDirection = NORTH;
+        } else if (directionY == -1) {
+            finalDirection = SOUTH;
+        }
+
+        return finalDirection;
+    }
+}
